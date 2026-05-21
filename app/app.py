dacc1c80b975d48d204be978ab23265c6b37ff2e
ch1ef21 <versininils06@gmail.com>
2026-05-21 14:35:52 +0300

    Добавление flask-cors чтобы не было ошибок в браузере при отправке запросов на порты

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
 import sys
 from flask import Flask, jsonify, request
 from models import ProductRepository
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 db_set_products = ProductRepository()
 
 def init_defaults():
