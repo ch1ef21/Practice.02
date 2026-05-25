2195623e77f69073e98e4036a19d4e12362da886
ch1ef21 <versininils06@gmail.com>
2026-05-25 15:08:17 +0300

    Добавление 2 теста

--- tests/benchmarkTest.py
+++ tests/benchmarkTest.py
@@ -9,14 +9,21 @@ NO_PROXY = {"http": None, "https": None}
 
 def test_integration_performance_benchmark():
     print("\n\n=== СТАРТ ЗАМЕРА ПРОИЗВОДИТЕЛЬНОСТИ ИНТЕГРАЦИИ ===")
-    
+
     t0 = time.perf_counter()
-    cat_res = requests.get(f"{CATALOG_URL}/products", proxies=NO_PROXY)
+    cat_res1 = requests.get(f"{CATALOG_URL}/products", proxies=NO_PROXY)
     t1 = time.perf_counter()
-    catalog_latency = (t1 - t0) * 1000
-    assert cat_res.status_code == 200
+    catalog_latency_cold = (t1 - t0) * 1000
+    assert cat_res1.status_code == 200
     
-    product_id = cat_res.json()[0]["id"]
+    product_id = cat_res1.json()[0]["id"]
+    
+
+    t_start = time.perf_counter()
+    cat_res2 = requests.get(f"{CATALOG_URL}/products", proxies=NO_PROXY)
+    t_end = time.perf_counter()
+    catalog_latency_hot = (t_end - t_start) * 1000
+    assert cat_res2.status_code == 200
     
     cart_payload = {"user_id": 100, "product_id": product_id, "quantity": 1}
     t2 = time.perf_counter()
@@ -24,7 +31,7 @@ def test_integration_performance_benchmark():
     t3 = time.perf_counter()
     cart_latency = (t3 - t2) * 1000
     assert cart_res.status_code in [200, 201]
-    
+
     order_payload = {"user_id": 100, "items": [{"product_id": product_id, "quantity": 1}]}
     t4 = time.perf_counter()
     order_res = requests.post(f"{ORDER_URL}/orders/create", json=order_payload, proxies=NO_PROXY)
@@ -33,8 +40,9 @@ def test_integration_performance_benchmark():
     assert order_res.status_code == 201
 
     print(f"\n[BENCHMARK] Результаты замера времени отклика (Latency):")
-    print(f"  - Catalog Service (GET /products):  {catalog_latency:.2f} ms")
+    print(f"  - Catalog (1-й запрос, Холодный):  {catalog_latency_cold:.2f} ms (Идёт в БД)")
+    print(f"  - Catalog (2-й запрос, Горячий):   {catalog_latency_hot:.2f} ms (Идёт в КЭШ 🚀)")
     print(f"  - Cart Service    (POST /cart/add): {cart_latency:.2f} ms")
     print(f"  - Order Service   (POST /orders):   {order_latency:.2f} ms")
-    print(f"  - Суммарное время синхронной фазы:  {catalog_latency + cart_latency + order_latency:.2f} ms")
+    print(f"  - Суммарное время синхронной фазы:  {catalog_latency_hot + cart_latency + order_latency:.2f} ms")
     print("==================================================\n")
